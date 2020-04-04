94b41031b01479ce9ba7a77e9b89f512746ebef7
François Beausoleil <francois@teksol.info>
2020-04-04 16:49:02 -0400

    Delete active JWT tokens for unregistered users

--- db/deploy/tables/active_jwt_tokens.sql
+++ db/deploy/tables/active_jwt_tokens.sql
@@ -8,7 +8,7 @@ BEGIN;
   CREATE TABLE public.active_jwt_tokens(
       id serial primary key
     , jid uuid unique default gen_random_uuid()
-    , email citext not null references users(email)
+    , email text not null references users(email) on update cascade on delete cascade
   );
 
   COMMENT ON TABLE public.active_jwt_tokens IS 'Records active JWT tokens in the wild. If a row does not exist here, then the token is invalid and must be rejected. This table is to be used in conjuction with PostgREST''s pre-request function, in order to confirm the validity of the token.';
